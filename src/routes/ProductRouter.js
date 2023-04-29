29f627b89fa02445ec9ee300f16fced244e3afc5
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-04-29 23:41:01 +0100

    Update product controller

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,21 +1,17 @@
 import express from "express";
 import { getProduct, getProductID, createProduct, updatedProduct, deleteProduct } from "../controllers/ProductController.js";
-import {isLoggedIn} from "../controllers/middleware.js"
+import {isLoggedIn, isAdmin} from "../controllers/middleware.js"
 
 const ProductRouter = express.Router();
 
 ProductRouter.get("/", isLoggedIn, getProduct);
 
-ProductRouter.get("/:id", getProductID);
+ProductRouter.get("/:id", isLoggedIn, getProductID);
 
-ProductRouter.post("/",createProduct);
+ProductRouter.post("/", isLoggedIn, createProduct);
 
-ProductRouter.put("/:id", updatedProduct);
+ProductRouter.put("/:id", isLoggedIn,updatedProduct);
 
-ProductRouter.delete("/:id", deleteProduct);
-
-// router.post('/user', register)
-
-// router.post('/login', login)
+ProductRouter.delete("/:id", isAdmin, deleteProduct);
 
 export default ProductRouter;
