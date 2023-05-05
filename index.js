58c1012e752c4fbf46a3bb5a08ee7e7c5b604a72
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-05-05 21:17:56 +0100

    Fix entity too large error

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ connectDB();
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb"}));
+app.use(express.urlencoded({ extended: true, limit: "50mb", parameterLimit: 50000 }));
 
 
 app.use('/products', ProductRouter);
